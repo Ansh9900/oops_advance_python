3a4643199e1ec5e34c40c109ff85b9cf29ff87ff
1ansh <anshbansal9900@gmail.com>
2026-06-15 22:33:59 +0530

    first

---
+++ advance_python.py
@@ -0,0 +1,24 @@
+
+#! lambda
+#var = lambda n:n%2==0
+#print(var(5))
+#!
+# var = lambda n:n**2
+# print(var(5))
+#!
+# var = lambda a,b:a>b
+# print(var(2,3))
+#!
+# var = lambda a,b: "hi" if a>b else "bye"
+# print(var(2,5))
+#!
+# var = lambda n :n*n if n%2==0 else n**3
+# print(var(4))
+#!
+# var = lambda n : n[::-1] if type(n)== str   or type(n)== list or type(n)== tuple else n
+# print(var([1,2,3]))
+# print(var(123))
+#!
+# var = lambda a,b:len(a) == len(b)
+# print(var("ansh","bansal"))
+#*day 2
---
+++ trash.py
@@ -0,0 +1,4 @@
+
+#! lambda
+a = lambda a,b,c: a a>b and a>c else b if b>c else c
+print(a(2,3,4))
